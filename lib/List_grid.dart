fa98f22f1e4d789751a2dd1ac078c212e0c9ce37
fareha-73 <farehaenny16@gmail.com>
2025-04-26 22:55:07 +0600

    Add files via upload

--- lib/List_grid.dart
+++ lib/List_grid.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class ListGrid extends StatefulWidget {
   const ListGrid({super.key});
@@ -22,22 +23,52 @@ class _ListGridState extends State<ListGrid> {
         backgroundColor: Colors.blue,
         elevation: 0,
       ),
-      body: SizedBox(
-        width: double.infinity,
-        child: ListView.builder(
-          itemCount: fruits.length,
-          itemBuilder: (context, index) {
-            return Card(
-              child: ListTile(
-                onTap: () {
-                  print((fruits_person['fruits'][index]));
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              width: double.infinity,
+              height: 300,
+              child: GridView.builder(
+                itemCount: fruits.length,
+                scrollDirection: Axis.horizontal,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: ListTile(
+                        onTap: () {
+                          print((fruits_person['fruits'][index]));
+                        },
+                        leading: Icon(Icons.food_bank),
+                        trailing: Icon(Icons.dangerous),
+                        title: Text(fruits_person['fruits'][index]),
+                        subtitle: Text(fruits_person['names'][index]),
+                      )
+                  );
                 },
-                leading: Icon(Icons.person),
-                title: Text(fruits_person['fruits'][index]),
-                subtitle: Text(fruits_person['names'][index]),
               ),
-            );
-          },
+            ),
+        
+            SizedBox(
+              width: double.infinity,
+              height: 300,
+              child: ListView.builder(
+                itemCount: fruits.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: ListTile(
+                      onTap: () {
+                        print((fruits_person['fruits'][index]));
+                      },
+                      leading: Icon(Icons.person),
+                      title: Text(fruits_person['fruits'][index]),
+                      subtitle: Text(fruits_person['names'][index]),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
